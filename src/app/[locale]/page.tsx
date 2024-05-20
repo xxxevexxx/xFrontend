c4db1b96064aa100720a60a7923b75a11fff426b
xxxevexxx <dl6l6l6lb@gmail.com>
2024-05-20 12:59:41 +0300

    xxx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,4 @@
+import { svgLogo, svgTelegram, svgVkontakte } from "@/components/SvgCollections";
 import React from "react";
 
 
@@ -5,10 +6,16 @@ const HomePage = () => {
     return (
         <div className="container relative">
             <section className="mx-auto flex max-w-[1240px] flex-col items-center">
-                <div>HomePage</div>
+                <div className="flex items-center gap-3 w-[300px]">
+                    <div className="left-0 h-[250px]">{svgTelegram()}</div>
+                    <div className="w-full border-t-[0.5px] top-[35px]"></div>
+                    <div className="h-17">{svgLogo()}</div>
+                    <div className="w-full border-t-[0.5px] top-[35px]"></div>
+                    <div className="right-0 h-[300px]">{svgVkontakte()}</div>
+                </div>
             </section>
         </div>
-    );
+    )
 }
 
 export default HomePage;
