caf91953699b191edee46a6b99347040ba7ac032
xxxevexxx <dl6l6l6lb@gmail.com>
2024-05-20 15:29:09 +0300

    xxx

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import {Pathnames} from 'next-intl/navigation';
 
-export const port = process.env.PORT || 3000;
+export const port = process.env.PORT || 3543;
 export const host = process.env.VERCEL_URL
     ? `https://${process.env.VERCEL_URL}`
     : `http://localhost:${port}`;
